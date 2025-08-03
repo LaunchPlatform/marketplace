eb67f1904cdff580be11db8f9c811ace45806fba
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-03 12:07:19 -0700

    Implement more of the produce

--- marketplace/training.py
+++ marketplace/training.py
@@ -1,17 +1,33 @@
 import dataclasses
 import typing
 
-import tinygrad
+from tinygrad import Tensor
 
 
 @dataclasses.dataclass
 class Spec:
-    models: list[typing.Callable[[tinygrad.Tensor], tinygrad.Tensor]]
-    vendors: int
+    vendors: list[typing.Callable[[Tensor], Tensor]]
     upstream_sampling: int
 
 
-def forward(
-    spec: Spec, data: tinygrad.Tensor, paths: tinygrad.Tensor | None = None
-) -> tuple[tinygrad.Tensor, tinygrad.Tensor]:
-    pass
+def produce(
+    spec: Spec, x: Tensor, paths: Tensor | None = None
+) -> tuple[Tensor, Tensor]:
+    if paths is None:
+        # this is the first spec for taking in the raw input, let's feed data to all of them
+        data = Tensor.stack(*(vendor(x) for vendor in spec.vendors), dim=0)
+        paths = Tensor.arange(len(spec.vendors)).unsqueeze(1)
+        return data, paths
+    if x.size(0) != paths.size(0):
+        raise ValueError(
+            "Provided input data's first dimension doesn't match with the paths' first dimension"
+        )
+
+    upstream_product_count = paths.size(0)
+    upstream_indexes = Tensor.stack(
+        *(
+            Tensor.randperm(upstream_product_count)[: spec.upstream_sampling]
+            for _ in range(len(spec.vendors))
+        ),
+        dim=0,
+    )
