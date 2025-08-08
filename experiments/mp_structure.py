31dd129498e8622120c5d00e53e2c36d85cd4b54
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-08 14:28:00 -0700

    Make exp ready

--- experiments/mp_structure.py
+++ experiments/mp_structure.py
@@ -7,7 +7,7 @@ from .utils import ensure_experiment
 
 logger = logging.getLogger(__name__)
 
-HOMOGENEOUS_STRUCTURE = [
+HOMOGENEOUS_HALF_UPSTREAM_STRUCTURE = [
     # layer 0
     (32, 0),
     # layer 1
@@ -23,7 +23,23 @@ HOMOGENEOUS_STRUCTURE = [
     # layer 6
     (32, 16),
 ]
-PYRAMID_STRUCTURE = [
+HOMOGENEOUS_FULL_UPSTREAM_STRUCTURE = [
+    # layer 0
+    (32, 0),
+    # layer 1
+    (32, 32),
+    # layer 2 (N/A)
+    (0, 0),
+    # layer 3
+    (32, 32),
+    # layer 4
+    (32, 32),
+    # layer 5 (N/A)
+    (0, 0),
+    # layer 6
+    (32, 32),
+]
+PYRAMID64_HALF_UPSTREAM_STRUCTURE = [
     # layer 0
     (4, 0),
     # layer 1
@@ -39,19 +55,83 @@ PYRAMID_STRUCTURE = [
     # layer 6
     (64, 32),
 ]
+PYRAMID64_FULL_UPSTREAM_STRUCTURE = [
+    # layer 0
+    (4, 0),
+    # layer 1
+    (8, 8),
+    # layer 2 (N/A)
+    (0, 0),
+    # layer 3
+    (16, 16),
+    # layer 4
+    (32, 32),
+    # layer 5 (N/A)
+    (0, 0),
+    # layer 6
+    (64, 64),
+]
+PYRAMID32_HALF_UPSTREAM_STRUCTURE = [
+    # layer 0
+    (2, 0),
+    # layer 1
+    (4, 2),
+    # layer 2 (N/A)
+    (0, 0),
+    # layer 3
+    (8, 4),
+    # layer 4
+    (16, 8),
+    # layer 5 (N/A)
+    (0, 0),
+    # layer 6
+    (32, 16),
+]
+PYRAMID32_FULL_UPSTREAM_STRUCTURE = [
+    # layer 0
+    (2, 0),
+    # layer 1
+    (4, 4),
+    # layer 2 (N/A)
+    (0, 0),
+    # layer 3
+    (8, 8),
+    # layer 4
+    (16, 16),
+    # layer 5 (N/A)
+    (0, 0),
+    # layer 6
+    (32, 32),
+]
 
 
 def main():
     exp_id = ensure_experiment("Marketplace Structure")
     for name, forward_pass, structure in [
-        ("homogeneous", 1, HOMOGENEOUS_STRUCTURE),
-        ("homogeneous", 2, HOMOGENEOUS_STRUCTURE),
-        ("homogeneous", 4, HOMOGENEOUS_STRUCTURE),
-        ("homogeneous", 8, HOMOGENEOUS_STRUCTURE),
-        ("pyramid", 1, PYRAMID_STRUCTURE),
-        ("pyramid", 2, PYRAMID_STRUCTURE),
-        ("pyramid", 4, PYRAMID_STRUCTURE),
-        ("pyramid", 8, PYRAMID_STRUCTURE),
+        ("homogeneous-half", 1, HOMOGENEOUS_HALF_UPSTREAM_STRUCTURE),
+        ("homogeneous-half", 2, HOMOGENEOUS_HALF_UPSTREAM_STRUCTURE),
+        ("homogeneous-half", 4, HOMOGENEOUS_HALF_UPSTREAM_STRUCTURE),
+        ("homogeneous-half", 8, HOMOGENEOUS_HALF_UPSTREAM_STRUCTURE),
+        ("homogeneous-full", 1, HOMOGENEOUS_FULL_UPSTREAM_STRUCTURE),
+        ("homogeneous-full", 2, HOMOGENEOUS_FULL_UPSTREAM_STRUCTURE),
+        ("homogeneous-full", 4, HOMOGENEOUS_FULL_UPSTREAM_STRUCTURE),
+        ("homogeneous-full", 8, HOMOGENEOUS_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid64-half", 1, PYRAMID64_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid64-half", 2, PYRAMID64_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid64-half", 4, PYRAMID64_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid64-half", 8, PYRAMID64_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid64-full", 1, PYRAMID64_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid64-full", 2, PYRAMID64_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid64-full", 4, PYRAMID64_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid64-full", 8, PYRAMID64_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid32-half", 1, PYRAMID32_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid32-half", 2, PYRAMID32_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid32-half", 4, PYRAMID32_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid32-half", 8, PYRAMID32_HALF_UPSTREAM_STRUCTURE),
+        ("pyramid32-full", 1, PYRAMID32_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid32-full", 2, PYRAMID32_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid32-full", 4, PYRAMID32_FULL_UPSTREAM_STRUCTURE),
+        ("pyramid32-full", 8, PYRAMID32_FULL_UPSTREAM_STRUCTURE),
     ]:
         with mlflow.start_run(
             run_name=f"{name}-fw{forward_pass}",
