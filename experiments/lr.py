a1845ca40679a6122babb916f5fc7e51808384c5
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-08 11:19:36 -0700

    Round 3 LR exp

--- experiments/lr.py
+++ experiments/lr.py
@@ -11,17 +11,17 @@ logger = logging.getLogger(__name__)
 def main():
     exp_id = ensure_experiment("Learning Rate")
     for lr, decay in [
-        (1e-3, 3.0e-4),
-        (1e-3, 3.5e-4),
-        (1e-3, 4.0e-4),
-        (1e-3, 4.5e-4),
-        (1e-3, 5.0e-4),
+        (1e-3, 4.500e-4),
+        (1e-3, 4.525e-4),
+        (1e-3, 4.550e-4),
+        (1e-3, 4.575e-4),
     ]:
         with mlflow.start_run(
-            run_name=f"lr-{lr:.1e}-decay-{decay:.1e}-round-2",
+            run_name=f"lr-{lr:.3e}-decay-{decay:.3e}-round-3",
             experiment_id=exp_id,
             description="Find out how learning rate and decay rate affects the training process",
             log_system_metrics=True,
+            tags=dict(round="3"),
         ):
             train(
                 step_count=10_000,
