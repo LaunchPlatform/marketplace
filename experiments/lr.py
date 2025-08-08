e7537c5bc186c6468fc057a2dc1fa28673d3fcad
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-08 10:00:30 -0700

    Remove comments

--- experiments/lr.py
+++ experiments/lr.py
@@ -11,25 +11,14 @@ logger = logging.getLogger(__name__)
 def main():
     exp_id = ensure_experiment("Learning Rate")
     for lr, decay in [
-        # (1e-3, 1e-4),
-        # (1e-3, 2e-4),
         (1e-3, 3.0e-4),
         (1e-3, 3.5e-4),
         (1e-3, 4.0e-4),
         (1e-3, 4.5e-4),
         (1e-3, 5.0e-4),
-        # (1e-3, 8e-4),
-        # (1e-4, 1e-4),
-        # (1e-4, 2e-4),
-        # (1e-4, 4e-4),
-        # (1e-4, 8e-4),
-        # (1e-5, 1e-4),
-        # (1e-5, 2e-4),
-        # (1e-5, 4e-4),
-        # (1e-5, 8e-4),
     ]:
         with mlflow.start_run(
-            run_name=f"lr-{lr:.0e}-decay-{decay:.0e}-round-2",
+            run_name=f"lr-{lr:.1e}-decay-{decay:.1e}-round-2",
             experiment_id=exp_id,
             description="Find out how learning rate and decay rate affects the training process",
             log_system_metrics=True,
