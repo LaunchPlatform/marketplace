b0fcc4a35babad64a7cd3d063c0144f008212171
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-12 11:56:42 -0700

    Batch normal and evolve enabled by default now

--- experiments/beautiful_mnist.py
+++ experiments/beautiful_mnist.py
@@ -37,8 +37,8 @@ def load_data():
 
 def make_marketplace(
     structure: list[tuple[int, int]] | None = None,
-    batch_normal: bool = False,
-    evolve_batch_normal: bool = False,
+    batch_normal: bool = True,
+    evolve_batch_normal: bool = True,
 ):
     if structure is None:
         structure = [
