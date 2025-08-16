e535c03bd52d3a6be097e7aac872d3aed288091e
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-16 15:39:22 -0700

    LR v5

--- experiments/lr.py
+++ experiments/lr.py
@@ -8,42 +8,24 @@ from .utils import ensure_experiment
 
 logger = logging.getLogger(__name__)
 
-PYRAMID32_HALF_UPSTREAM_STRUCTURE = [
-    # layer 0
-    (2, 0),
-    # layer 1
-    (4, 2),
-    # layer 2
-    (4, 4),
-    # layer 3
-    (8, 4),
-    # layer 4
-    (16, 8),
-    # layer 5
-    (16, 16),
-    # layer 6
-    (32, 16),
-]
-
 
 def main():
-    exp_id = ensure_experiment("Learning Rate V4")
-    for batch_size in [32, 64, 128, 256, 512]:
-        for lr in [1e-2, 1e-3, 1e-4]:
-            for decay in [1e-3, 1e-4, 1e-5]:
-                with mlflow.start_run(
-                    run_name=f"bs-{batch_size}-lr-{lr:.1e}-decay-{decay:.1e}",
-                    experiment_id=exp_id,
-                    log_system_metrics=True,
-                ):
-                    marketplace = make_marketplace(default_vendor_count=4)
-                    train(
-                        step_count=1_000,
-                        batch_size=batch_size,
-                        initial_lr=lr,
-                        lr_decay_rate=decay,
-                        marketplace=marketplace,
-                    )
+    exp_id = ensure_experiment("Learning Rate V5")
+    for lr in [1e-2, 1e-3, 1e-4]:
+        for decay in [1e-3, 1e-4, 1e-5]:
+            with mlflow.start_run(
+                run_name=f"bs-512-lr-{lr:.1e}-decay-{decay:.1e}",
+                experiment_id=exp_id,
+                log_system_metrics=True,
+            ):
+                marketplace = make_marketplace(default_vendor_count=8)
+                train(
+                    step_count=3_000,
+                    batch_size=512,
+                    initial_lr=lr,
+                    lr_decay_rate=decay,
+                    marketplace=marketplace,
+                )
 
 
 if __name__ == "__main__":
