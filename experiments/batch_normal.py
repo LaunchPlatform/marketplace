b00768fc1e47f3c8106ab3cb6e5b3362a6d4f975
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-16 22:15:52 -0700

    batch normal vs instance normal

--- experiments/batch_normal.py
+++ experiments/batch_normal.py
@@ -5,59 +5,26 @@ import mlflow
 from .beautiful_mnist import make_marketplace
 from .beautiful_mnist import train
 from .utils import ensure_experiment
+from marketplace.multi_nn import MultiBatchNorm
+from marketplace.multi_nn import MultiInstanceNorm
 
 logger = logging.getLogger(__name__)
 
-PYRAMID32_HALF_UPSTREAM_STRUCTURE = [
-    # layer 0
-    (2, 0),
-    # layer 1
-    (4, 2),
-    # layer 2 (N/A)
-    (4, 4),
-    # layer 3
-    (8, 4),
-    # layer 4
-    (16, 8),
-    # layer 5 (N/A)
-    (16, 16),
-    # layer 6
-    (32, 16),
-]
-
 
 def main():
-    exp_id = ensure_experiment("Batch Normal")
-    for batch_size, batch_normal, evolve in [
-        # (32, False, False),
-        # (32, True, False),
-        (32, True, True),
-        # (64, False, False),
-        # (64, True, False),
-        (64, True, True),
-        # (128, False, False),
-        # (128, True, False),
-        (128, True, True),
-        # (256, False, False),
-        # (256, True, False),
-        (256, True, True),
-        # (512, False, False),
-        # (512, True, False),
-        (512, True, True),
-    ]:
+    exp_id = ensure_experiment("Batch Normal vs Instance Normal")
+    for batch_normal in [True, False]:
         with mlflow.start_run(
-            run_name=f"normal-batch-{batch_size}-nb-{batch_normal}-evolve-{evolve}",
+            run_name="batch-normal" if batch_normal else "instance-normal",
             experiment_id=exp_id,
             log_system_metrics=True,
         ):
             marketplace = make_marketplace(
-                PYRAMID32_HALF_UPSTREAM_STRUCTURE,
-                batch_normal=batch_normal,
-                evolve_batch_normal=evolve,
+                norm_cls=MultiBatchNorm if batch_normal else MultiInstanceNorm
             )
             train(
                 step_count=10_000,
-                batch_size=batch_size,
+                batch_size=512,
                 initial_forward_pass=1,
                 initial_lr=1e-3,
                 lr_decay_rate=1e-4,
