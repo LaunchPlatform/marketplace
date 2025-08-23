5f8f5cae7005b785c9c7b11394fa8d272817a69e
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-22 19:01:26 -0700

    Add more tests

--- tests/test_random.py
+++ tests/test_random.py
@@ -1,15 +1,17 @@
 import pytest
 from tinygrad import dtypes
 from tinygrad import Tensor
+from tinygrad.helpers import ceildiv
+from tinygrad.helpers import prod
 
 from marketplace.random import rand
 
 
 @pytest.mark.parametrize(
-    "shape, seed, base_count, expected",
+    "shape, seed, counter, expected",
     [
         (
-            (5,),
+            (6,),
             Tensor(123456, dtype=dtypes.uint64),
             0,
             [
@@ -18,10 +20,11 @@ from marketplace.random import rand
                 0.4398590326309204,
                 0.6165577173233032,
                 0.04700958728790283,
+                0.5229370594024658,
             ],
         ),
         (
-            (5,),
+            (6,),
             Tensor(123456, dtype=dtypes.uint64),
             2,
             [
@@ -30,11 +33,25 @@ from marketplace.random import rand
                 0.35901951789855957,
                 0.5229370594024658,
                 0.39503049850463867,
+                0.4783148765563965,
+            ],
+        ),
+        (
+            (3, 2),
+            Tensor(123456, dtype=dtypes.uint64),
+            0,
+            [
+                [0.5452135801315308, 0.28107452392578125],
+                [0.4398590326309204, 0.6165577173233032],
+                [0.04700958728790283, 0.5229370594024658],
             ],
         ),
     ],
 )
-def test_rand(shape: tuple[int, ...], seed: Tensor, base_count: int, expected: list):
-    nums = rand(*shape, seed=seed, base_count=base_count)
-    print(nums.tolist())
+def test_rand(shape: tuple[int, ...], seed: Tensor, counter: int, expected: list):
+    counter_val = Tensor(counter)
+    nums = rand(*shape, seed=seed, counter=counter_val)
     assert nums.tolist() == expected
+    assert (
+        counter_val.item() == ceildiv(prod(shape) * dtypes.float.itemsize, 4) + counter
+    )
