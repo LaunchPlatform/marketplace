9dd443957162bdf425c277df610dc049c867ef7b
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-22 17:13:26 -0700

    Add test for random

---
+++ tests/test_random.py
@@ -0,0 +1,25 @@
+import pytest
+from tinygrad import dtypes
+from tinygrad import Tensor
+
+from marketplace.random import rand
+
+
+@pytest.mark.parametrize(
+    "shape, seed, base_count, expected",
+    [
+        (
+            (3, 2),
+            Tensor([123, 456], dtype=dtypes.uint32),
+            0,
+            [
+                [0.5470587015151978, 0.5954171419143677],
+                [0.2251112461090088, 0.45440757274627686],
+                [0.040180206298828125, 0.039162516593933105],
+            ],
+        )
+    ],
+)
+def test_rand(shape: tuple[int, ...], seed: Tensor, base_count: int, expected: list):
+    nums = rand(*shape, seed=seed, base_count=base_count)
+    assert nums.tolist() == expected
