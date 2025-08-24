0cc3ace400c3ab526e26d6ab3d58561abda59cef
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-24 12:29:40 -0700

    Add StochasticOptimizer

--- marketplace/optimizers.py
+++ marketplace/optimizers.py
@@ -4,10 +4,14 @@ from collections import OrderedDict
 
 from tinygrad import dtypes
 from tinygrad import Tensor
+from tinygrad.nn.state import get_parameters
 from tinygrad.nn.state import get_state_dict
 from tinygrad.nn.state import load_state_dict
 
 from .random import rand
+from .training import Spec
+
+SEED_MAX = 2**64
 
 
 class StochasticVendor:
@@ -40,6 +44,8 @@ class StochasticVendor:
     def make_delta(self, params: Tensor) -> Tensor:
         high = self.learning_rate
         low = -self.learning_rate
+        # TODO: take RNG from the external to make it possible to use different RNG algorithm?
+        # TODO: deal with zero seed
         return (
             (high - low) * rand(*params.shape, seed=self.seed, counter=self.counter)
         ).cast(params.dtype or dtypes.default_float) + low
@@ -54,3 +60,45 @@ class StochasticVendor:
     def persist(self):
         # TODO: persist delta to model params
         pass
+
+
+class StochasticOptimizer:
+    def __init__(self, marketplace: list[Spec], learning_rate: Tensor):
+        self.marketplace = marketplace
+        self.learning_rate = learning_rate
+
+        self.seeds = [
+            Tensor.randint(spec.vendor_count, low=1, high=SEED_MAX, dtype=dtypes.uint64)
+            for spec in self.marketplace
+        ]
+        Tensor.realize(
+            *(
+                # We need to realize all the parameters so that they are buffer instead of compute graph, otherwise the assign
+                # operation won't work.
+                # ref: https://x.com/fangpenlin/status/1959405151455969607
+                [
+                    param
+                    for spec in self.marketplace
+                    for param in get_parameters(spec.model)
+                ]
+                # also realize seeds so that they are buffer
+                + self.seeds
+            )
+        )
+        self.vendors = [
+            [
+                StochasticVendor(seed=seed, learning_rate=self.learning_rate)
+                for seed in vendor_seeds
+            ]
+            for vendor_seeds in self.seeds
+        ]
+
+    def step(self, seeds: Tensor):
+        Tensor.realize(*self.schedule_step(seeds))
+
+    def schedule_step(self, seeds: Tensor) -> list[Tensor]:
+        return [
+            param
+            for spec, seed in zip(self.marketplace, seeds)
+            for param in spec.model.update(self.make_rng(seed)).values()
+        ]
