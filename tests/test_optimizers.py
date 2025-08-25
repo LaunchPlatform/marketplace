fd170966d1e0f50f46193a405b61b448fed0f64f
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-24 17:15:21 -0700

    More tests

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -5,7 +5,7 @@ from marketplace.optimizers import DeltaVendor
 
 class Multiply:
     def __init__(self, number: float):
-        self.number = Tensor(number).realize()
+        self.number = Tensor(number).contiguous().realize()
 
     def __call__(self, x: Tensor) -> Tensor:
         return x * self.number
@@ -19,3 +19,7 @@ def test_delta_vendor():
     assert vendor(x).item() == 32.0
     # ensure that we didn't change the weights of original model
     assert model(x).item() == 12.0
+
+    model.number.assign(4.0)
+    assert model(x).item() == 16.0
+    assert vendor(x).item() == 36.0
