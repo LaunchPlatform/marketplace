9c2a50e9f332662c00b095bbea5a518d56a57d72
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-26 11:45:18 -0700

    Fix lr

--- marketplace/optimizers.py
+++ marketplace/optimizers.py
@@ -214,6 +214,9 @@ class Optimizer:
                         + self.make_delta(
                             seed=seed,
                             counter=Tensor(counter, dtype=dtypes.uint),
+                            lr=self.learning_rate
+                            if self.meta_learning_rate is None
+                            else ctx.learning_rate,
                             params=params,
                         )
                     )
