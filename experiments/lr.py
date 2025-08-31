e2595e984bf905b0c8526debc37538a1d2d25473
Fang-Pen Lin <hello@fangpenlin.com>
2025-08-31 00:36:20 -0700

    More for lr exp

--- experiments/lr.py
+++ experiments/lr.py
@@ -8,24 +8,29 @@ from .utils import ensure_experiment
 
 logger = logging.getLogger(__name__)
 
+VENDOR_COUNT = 8
+
 
 def main():
     exp_id = ensure_experiment("Param Attribution LR")
-    for lr in [1e-1, 1e-2, 1e-3, 1e-4]:
-        for decay in [1e-3, 1e-4, 1e-5]:
-            with mlflow.start_run(
-                run_name=f"bs-512-lr-{lr:.1e}-decay-{decay:.1e}",
-                experiment_id=exp_id,
-                log_system_metrics=True,
-            ):
-                marketplace = make_marketplace(default_vendor_count=8)
-                train(
-                    step_count=3_000,
-                    batch_size=512,
-                    initial_lr=lr,
-                    lr_decay_rate=decay,
-                    marketplace=marketplace,
-                )
+    for probe in [1e-1, 1e-2, 1e-3, 1e-4]:
+        for lr in [1e-1, 1e-2, 1e-3, 1e-4]:
+            for decay in [1e-3, 1e-4, 1e-5]:
+                with mlflow.start_run(
+                    run_name=f"lr-{lr:.1e}-decay-{decay:.1e}",
+                    experiment_id=exp_id,
+                    log_system_metrics=True,
+                ):
+                    marketplace = make_marketplace(default_vendor_count=VENDOR_COUNT)
+                    mlflow.log_param("vendor_count", VENDOR_COUNT)
+                    train(
+                        step_count=1_000,
+                        batch_size=512,
+                        initial_lr=lr,
+                        lr_decay_rate=decay,
+                        probe=probe,
+                        marketplace=marketplace,
+                    )
 
 
 if __name__ == "__main__":
