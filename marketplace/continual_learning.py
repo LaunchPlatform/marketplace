707a37f3aff35d2e79a1cb07a90597cf7c803b73
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-03 21:39:27 -0700

    xxx

--- marketplace/continual_learning.py
+++ marketplace/continual_learning.py
@@ -13,12 +13,12 @@ def forward_with_paths(
     output = []
     # TODO: this is extremely slow for Tinygrad JIT compiler, should find a better way to do it instead
     for xi, path in zip(x, paths):
-        data = xi
+        data = xi.unsqueeze(0)
         for spec, delta, idx in zip(marketplace, deltas, path):
             vendor = CachedDeltaVendor(
                 model=spec.model,
                 delta={key: params[idx] for key, params in delta.items()},
             )
             data = vendor(data)
-        output.append(data)
+        output.append(data[0])
     return Tensor.stack(*output, dim=0)
