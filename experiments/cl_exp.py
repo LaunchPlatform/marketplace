252f69fe584b972b000f768dbc8ef6ae491e519c
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-03 23:09:37 -0700

    Add cl exp

---
+++ experiments/cl_exp.py
@@ -0,0 +1,38 @@
+import logging
+import pathlib
+
+import mlflow
+
+from .beautiful_mnist import make_marketplace
+from .beautiful_mnist import train
+from .utils import ensure_experiment
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    exp_id = ensure_experiment("Continual Learning")
+    checkpoint_file = pathlib.Path("continual-learning.safetensors")
+    if not checkpoint_file.exists():
+        train_vendor_count = 16
+        with mlflow.start_run(
+            run_name="train-base-model",
+            experiment_id=exp_id,
+            log_system_metrics=True,
+        ):
+            marketplace = make_marketplace(default_vendor_count=train_vendor_count)
+            mlflow.log_param("vendor_count", train_vendor_count)
+            train(
+                step_count=1_000,
+                batch_size=512,
+                initial_lr=1e-1,
+                lr_decay_rate=1e-5,
+                probe_scale=1e-1,
+                marketplace=marketplace,
+                manual_seed=42,
+            )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
