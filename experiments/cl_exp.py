9ac6ffb998695392904273df3776dec464f931fc
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-04 10:44:48 -0700

    v2 exp

--- experiments/cl_exp.py
+++ experiments/cl_exp.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    exp_id = ensure_experiment("Continual Learning")
+    exp_id = ensure_experiment("Continual Learning V2")
     checkpoint_file = pathlib.Path("continual-learning.safetensors")
     if not checkpoint_file.exists():
         train_vendor_count = 16
@@ -37,30 +37,29 @@ def main():
         logger.info("Checkpoint file %s already exists, skip", checkpoint_file)
     for learn_vendor_count in [4, 8, 16]:
         for fw in [1, 2, 4, 8, 16]:
-            for lr in [1e-2, 3e-2, 5e-2, 7e-2, 1e-1, 2e-1]:
-                for probe_scale in [1, 1e-1, 1e-2, 1e-3]:
-                    with mlflow.start_run(
-                        run_name=f"learn-vendor-{learn_vendor_count}-lr-{lr:.1e}-fw-{fw}-probe-scale-{probe_scale:.1e}",
-                        experiment_id=exp_id,
-                        log_system_metrics=True,
-                    ):
-                        marketplace = make_marketplace(
-                            default_vendor_count=learn_vendor_count
-                        )
-                        mlflow.log_param("vendor_count", learn_vendor_count)
-                        learn(
-                            step_count=10_000,
-                            batch_size=256,
-                            target_new_classes=(3,),
-                            new_train_size=32,
-                            initial_lr=lr,
-                            lr_decay_rate=0,
-                            probe_scale=probe_scale,
-                            forward_pass=fw,
-                            marketplace=marketplace,
-                            manual_seed=42,
-                            input_checkpoint_filepath=checkpoint_file,
-                        )
+            for lr in [9e-3, 1e-2, 2e-2, 3e-2, 1e-1, 2e-1]:
+                with mlflow.start_run(
+                    run_name=f"learn-vendor-{learn_vendor_count}-lr-{lr:.1e}-fw-{fw}",
+                    experiment_id=exp_id,
+                    log_system_metrics=True,
+                ):
+                    marketplace = make_marketplace(
+                        default_vendor_count=learn_vendor_count
+                    )
+                    mlflow.log_param("vendor_count", learn_vendor_count)
+                    learn(
+                        step_count=10_000,
+                        batch_size=256,
+                        target_new_classes=(3,),
+                        new_train_size=32,
+                        initial_lr=lr,
+                        lr_decay_rate=0,
+                        probe_scale=1.0,
+                        forward_pass=fw,
+                        marketplace=marketplace,
+                        manual_seed=42,
+                        input_checkpoint_filepath=checkpoint_file,
+                    )
 
 
 if __name__ == "__main__":
