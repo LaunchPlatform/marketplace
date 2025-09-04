0e2d37055873944e434e45250ef73ce4a15358e9
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-04 01:28:02 -0700

    xxx

--- experiments/cl_exp.py
+++ experiments/cl_exp.py
@@ -35,29 +35,31 @@ def main():
             )
     else:
         logger.info("Checkpoint file %s already exists, skip", checkpoint_file)
-    for fw in [1, 2, 4, 8, 16]:
-        for lr in [1e-2, 3e-2, 5e-2, 7e-2]:
-            with mlflow.start_run(
-                run_name=f"learn-lr-{lr:.1e}-fw-{fw}",
-                experiment_id=exp_id,
-                log_system_metrics=True,
-            ):
-                learn_vendor_count = 16
-                marketplace = make_marketplace(default_vendor_count=learn_vendor_count)
-                mlflow.log_param("vendor_count", learn_vendor_count)
-                learn(
-                    step_count=10_000,
-                    batch_size=256,
-                    target_new_classes=(3,),
-                    new_train_size=32,
-                    initial_lr=lr,
-                    lr_decay_rate=0,
-                    probe_scale=1e-1,
-                    forward_pass=fw,
-                    marketplace=marketplace,
-                    manual_seed=42,
-                    input_checkpoint_filepath=checkpoint_file,
-                )
+    for learn_vendor_count in [4, 8, 16]:
+        for fw in [1, 2, 4, 8, 16]:
+            for lr in [1e-2, 3e-2, 5e-2, 7e-2]:
+                with mlflow.start_run(
+                    run_name=f"learn-vendor-{learn_vendor_count}-lr-{lr:.1e}-fw-{fw}",
+                    experiment_id=exp_id,
+                    log_system_metrics=True,
+                ):
+                    marketplace = make_marketplace(
+                        default_vendor_count=learn_vendor_count
+                    )
+                    mlflow.log_param("vendor_count", learn_vendor_count)
+                    learn(
+                        step_count=10_000,
+                        batch_size=256,
+                        target_new_classes=(3,),
+                        new_train_size=32,
+                        initial_lr=lr,
+                        lr_decay_rate=0,
+                        probe_scale=1e-1,
+                        forward_pass=fw,
+                        marketplace=marketplace,
+                        manual_seed=42,
+                        input_checkpoint_filepath=checkpoint_file,
+                    )
 
 
 if __name__ == "__main__":
