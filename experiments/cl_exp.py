c336ec382e10044591b7e2aa8ad4ec31bbad2575
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-03 23:21:12 -0700

    xxx

--- experiments/cl_exp.py
+++ experiments/cl_exp.py
@@ -5,6 +5,7 @@ import mlflow
 
 from .beautiful_mnist import make_marketplace
 from .beautiful_mnist import train
+from .continual_learning import learn
 from .utils import ensure_experiment
 
 logger = logging.getLogger(__name__)
@@ -16,7 +17,7 @@ def main():
     if not checkpoint_file.exists():
         train_vendor_count = 16
         with mlflow.start_run(
-            run_name="train-base-model",
+            run_name="base-model",
             experiment_id=exp_id,
             log_system_metrics=True,
         ):
@@ -32,6 +33,23 @@ def main():
                 manual_seed=42,
                 checkpoint_filepath=checkpoint_file,
             )
+    with mlflow.start_run(
+        run_name="learn",
+        experiment_id=exp_id,
+        log_system_metrics=True,
+    ):
+        learn(
+            step_count=2_000,
+            batch_size=64,
+            target_new_classes=(3,),
+            new_train_size=8,
+            initial_lr=1e-1,
+            lr_decay_rate=1e-5,
+            probe_scale=1e-1,
+            marketplace=marketplace,
+            manual_seed=42,
+            input_checkpoint_filepath=checkpoint_file,
+        )
 
 
 if __name__ == "__main__":
