ab01437828ba5a5f505f5de2c11e47104e560430
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-07 23:54:28 -0700

    xxx

--- plot/plot_continual_learning_fashion.py
+++ plot/plot_continual_learning_fashion.py
@@ -233,7 +233,7 @@ if __name__ == "__main__":
 
     def make_frame(kwargs):
         plot_frame(**kwargs)
-        return prepare_kwargs["output_file"]
+        return kwargs["output_file"]
 
     # ref: https://stackoverflow.com/a/6191991
     def init_worker():
